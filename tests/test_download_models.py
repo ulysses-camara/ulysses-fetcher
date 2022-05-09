12ac4798ac29b56bab8a86f121091e4c9d677abb
felsiq <felipe.siqueira@usp.br>
2022-05-08 21:12:43 -0300

    tests: add test files

---
+++ tests/test_download_models.py
@@ -0,0 +1,107 @@
+import os
+import shutil
+import glob
+
+import pytest
+import time
+
+import buscador
+
+
+@pytest.mark.parametrize(
+    "model_name",
+    [
+        "2_layer_6000_vocab_size_bert",
+        "512_hidden_dim_6000_vocab_size_1_layer_lstm",
+        "6000_subword_tokenizer",
+    ],
+)
+def test_download_segmenter_models(model_name: str):
+    output_dir = os.path.join(os.path.dirname(__file__), "downloaded_models_from_tests")
+
+    has_succeed = buscador.download_model(
+        task_name="legal_text_segmentation",
+        model_name=model_name,
+        output_dir=output_dir,
+        show_progress_bar=True,
+        check_cached=False,
+        clean_zip_files=True,
+        check_model_hash=True,
+    )
+
+    output_uri = os.path.join(output_dir, model_name)
+
+    assert has_succeed and (os.path.exists(output_uri) or os.path.exists(f"{output_uri}.pt"))
+
+    t_start = time.perf_counter()
+
+    has_succeed = buscador.download_model(
+        task_name="legal_text_segmentation",
+        model_name=model_name,
+        output_dir=output_dir,
+        show_progress_bar=True,
+        check_cached=True,
+        clean_zip_files=True,
+        check_model_hash=True,
+    )
+
+    t_delta = time.perf_counter() - t_start
+
+    assert t_delta <= 1.0
+    assert has_succeed and (os.path.exists(output_uri) or os.path.exists(f"{output_uri}.pt"))
+
+    if os.path.isdir(output_uri):
+        shutil.rmtree(output_dir)
+
+    else:
+        for uri in glob.glob(f"{output_uri}*"):
+            os.remove(uri)
+
+        os.rmdir(output_dir)
+
+
+def test_keep_zip_file():
+    model_name = "6000_subword_tokenizer"
+    output_dir = os.path.join(os.path.dirname(__file__), "downloaded_models_from_tests")
+
+    has_succeed = buscador.download_model(
+        task_name="legal_text_segmentation",
+        model_name=model_name,
+        output_dir=output_dir,
+        show_progress_bar=True,
+        check_cached=False,
+        clean_zip_files=False,
+        check_model_hash=True,
+    )
+
+    output_uri = os.path.join(output_dir, model_name)
+
+    assert has_succeed and os.path.exists(output_uri)
+    assert os.path.isfile(os.path.join(output_dir, f"{model_name}.zip"))
+
+    t_start = time.perf_counter()
+
+    has_succeed = buscador.download_model(
+        task_name="legal_text_segmentation",
+        model_name=model_name,
+        output_dir=output_dir,
+        show_progress_bar=True,
+        check_cached=True,
+        clean_zip_files=False,
+        check_model_hash=True,
+    )
+
+    t_delta = time.perf_counter() - t_start
+
+    assert t_delta <= 1.0
+    assert has_succeed and os.path.exists(output_uri)
+
+    shutil.rmtree(output_dir)
+
+
+def test_get_available_tasks():
+    assert buscador.get_available_tasks()
+
+
+def test_get_available_models_for_segmentation_task():
+    assert buscador.get_task_available_models(task_name="legal_text_segmentation")
---
+++ tests/test_errors_and_warnings.py
@@ -0,0 +1,51 @@
+import os
+
+import pytest
+import pytest_socket
+
+import buscador
+
+
+def test_unknown_task_name():
+    with pytest.raises(ValueError):
+        output_dir = "should_not_exists_dir"
+
+        buscador.download_model(
+            task_name="unknown_task_name",
+            model_name="6000_subword_tokenizer",
+            output_dir=output_dir,
+            show_progress_bar=False,
+        )
+
+    assert not os.path.isdir(output_dir)
+
+
+def test_unknown_model_name():
+    with pytest.raises(ValueError):
+        output_dir = "should_not_exists_dir"
+
+        buscador.download_model(
+            task_name="legal_text_segmentation",
+            model_name="unknown_model_name",
+            output_dir=output_dir,
+            show_progress_bar=False,
+        )
+
+    assert not os.path.isdir(output_dir)
+
+
+def test_disabled_connection():
+    pytest_socket.disable_socket()
+
+    with pytest.warns(RuntimeWarning, match="Could not download pretrained model"):
+        output_dir = "should_not_exists"
+
+        buscador.download_model(
+            task_name="legal_text_segmentation",
+            model_name="6000_subword_tokenizer",
+            output_dir=output_dir,
+            show_progress_bar=False,
+            check_cached=False,
+        )
+
+    assert not os.path.isdir(output_dir)
