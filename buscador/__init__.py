1a0702e4666dc8f7b2581cbd74435f96d3f8cd00
felsiq <felipe.siqueira@usp.br>
2022-05-10 23:05:00 -0300

    update: add version info in __init__.py

--- buscador/__init__.py
+++ buscador/__init__.py
@@ -1,2 +1,16 @@
 # pylint: disable='missing-module-docstring'
 from .download_models import *
+
+
+try:
+    import importlib.metadata as importlib_metadata
+
+except ModuleNotFoundError:
+    import importlib_metadata  # type: ignore
+
+
+try:
+    __version__ = importlib_metadata.version(__name__)
+
+except importlib_metadata.PackageNotFoundError:
+    __version__ = "0.1.1"
