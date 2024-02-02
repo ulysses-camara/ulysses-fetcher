171ce2af564a72390da574ec4a1b1d11121fd552
felsiq <felipe.siqueira@usp.br>
2024-02-02 18:09:34 -0300

    tests: add tests for 'sentence_model_evaluation'

--- tests/test_download_models.py
+++ tests/test_download_models.py
@@ -30,60 +30,78 @@ def test_trusted_urls_integrity():
 @pytest.mark.parametrize(
     "resource_name,task_name",
     [
-        ("2_layer_6000_vocab_size_bert", "legal_text_segmentation"),
-        ("512_hidden_dim_6000_vocab_size_1_layer_lstm", "legal_text_segmentation"),
-        ("6000_subword_tokenizer", "legal_text_segmentation"),
-        ("distil_sbert_br_ctimproved_12_epochs_v1", "sentence_similarity"),
-        ("dataset_wikipedia_ptbr_bigram_shift_v1", "probing_task"),
-        ("dataset_wikipedia_ptbr_coordination_inversion_v1", "probing_task"),
-        ("dataset_wikipedia_ptbr_obj_number_v1", "probing_task"),
-        ("dataset_wikipedia_ptbr_odd_man_out_v1", "probing_task"),
-        ("dataset_wikipedia_ptbr_past_present_v1", "probing_task"),
-        ("dataset_wikipedia_ptbr_sentence_length_v1", "probing_task"),
-        ("dataset_wikipedia_ptbr_subj_number_v1", "probing_task"),
-        ("dataset_wikipedia_ptbr_top_constituents_v1", "probing_task"),
-        ("dataset_wikipedia_ptbr_tree_depth_v1", "probing_task"),
-        ("dataset_wikipedia_ptbr_word_content_v1", "probing_task"),
-        ("dataset_sp_court_cases_bigram_shift_v1", "probing_task"),
-        ("dataset_sp_court_cases_coordination_inversion_v1", "probing_task"),
-        ("dataset_sp_court_cases_obj_number_v1", "probing_task"),
-        ("dataset_sp_court_cases_odd_man_out_v1", "probing_task"),
-        ("dataset_sp_court_cases_past_present_v1", "probing_task"),
-        ("dataset_sp_court_cases_sentence_length_v1", "probing_task"),
-        ("dataset_sp_court_cases_subj_number_v1", "probing_task"),
-        ("dataset_sp_court_cases_top_constituents_v1", "probing_task"),
-        ("dataset_sp_court_cases_tree_depth_v1", "probing_task"),
-        ("dataset_sp_court_cases_word_content_v1", "probing_task"),
-        ("dataset_political_speeches_ptbr_bigram_shift_v1", "probing_task"),
-        ("dataset_political_speeches_ptbr_coordination_inversion_v1", "probing_task"),
-        ("dataset_political_speeches_ptbr_obj_number_v1", "probing_task"),
-        ("dataset_political_speeches_ptbr_odd_man_out_v1", "probing_task"),
-        ("dataset_political_speeches_ptbr_past_present_v1", "probing_task"),
-        ("dataset_political_speeches_ptbr_sentence_length_v1", "probing_task"),
-        ("dataset_political_speeches_ptbr_subj_number_v1", "probing_task"),
-        ("dataset_political_speeches_ptbr_top_constituents_v1", "probing_task"),
-        ("dataset_political_speeches_ptbr_tree_depth_v1", "probing_task"),
-        ("dataset_political_speeches_ptbr_word_content_v1", "probing_task"),
-        ("dataset_leg_pop_comments_ptbr_bigram_shift_v1", "probing_task"),
-        ("dataset_leg_pop_comments_ptbr_coordination_inversion_v1", "probing_task"),
-        ("dataset_leg_pop_comments_ptbr_obj_number_v1", "probing_task"),
-        ("dataset_leg_pop_comments_ptbr_odd_man_out_v1", "probing_task"),
-        ("dataset_leg_pop_comments_ptbr_past_present_v1", "probing_task"),
-        ("dataset_leg_pop_comments_ptbr_sentence_length_v1", "probing_task"),
-        ("dataset_leg_pop_comments_ptbr_subj_number_v1", "probing_task"),
-        ("dataset_leg_pop_comments_ptbr_top_constituents_v1", "probing_task"),
-        ("dataset_leg_pop_comments_ptbr_tree_depth_v1", "probing_task"),
-        ("dataset_leg_pop_comments_ptbr_word_content_v1", "probing_task"),
-        ("dataset_leg_docs_ptbr_bigram_shift_v1", "probing_task"),
-        ("dataset_leg_docs_ptbr_coordination_inversion_v1", "probing_task"),
-        ("dataset_leg_docs_ptbr_obj_number_v1", "probing_task"),
-        ("dataset_leg_docs_ptbr_odd_man_out_v1", "probing_task"),
-        ("dataset_leg_docs_ptbr_past_present_v1", "probing_task"),
-        ("dataset_leg_docs_ptbr_sentence_length_v1", "probing_task"),
-        ("dataset_leg_docs_ptbr_subj_number_v1", "probing_task"),
-        ("dataset_leg_docs_ptbr_top_constituents_v1", "probing_task"),
-        ("dataset_leg_docs_ptbr_tree_depth_v1", "probing_task"),
-        ("dataset_leg_docs_ptbr_word_content_v1", "probing_task"),
+        ("bill_summary_to_topics", "sentence_model_evaluation"),
+        ("code_estatutes_cf88", "sentence_model_evaluation"),
+        ("factnews_news_bias", "sentence_model_evaluation"),
+        ("factnews_news_factuality", "sentence_model_evaluation"),
+        ("fakebr_size_normalized", "sentence_model_evaluation"),
+        ("faqs", "sentence_model_evaluation"),
+        ("hatebr_offensive_lang", "sentence_model_evaluation"),
+        ("masked_law_name_in_news", "sentence_model_evaluation"),
+        ("masked_law_name_in_summaries", "sentence_model_evaluation"),
+        ("oab_first_part", "sentence_model_evaluation"),
+        ("oab_second_part", "sentence_model_evaluation"),
+        ("offcombr2", "sentence_model_evaluation"),
+        ("stj_summary", "sentence_model_evaluation"),
+        ("sts_state_news", "sentence_model_evaluation"),
+        ("summary_vs_bill", "sentence_model_evaluation"),
+        ("tampered_leg", "sentence_model_evaluation"),
+        ("trf_examinations", "sentence_model_evaluation"),
+        ("ulysses_sd", "sentence_model_evaluation"),
+        # ("2_layer_6000_vocab_size_bert", "legal_text_segmentation"),
+        # ("512_hidden_dim_6000_vocab_size_1_layer_lstm", "legal_text_segmentation"),
+        # ("6000_subword_tokenizer", "legal_text_segmentation"),
+        # ("distil_sbert_br_ctimproved_12_epochs_v1", "sentence_similarity"),
+        # ("dataset_wikipedia_ptbr_bigram_shift_v1", "probing_task"),
+        # ("dataset_wikipedia_ptbr_coordination_inversion_v1", "probing_task"),
+        # ("dataset_wikipedia_ptbr_obj_number_v1", "probing_task"),
+        # ("dataset_wikipedia_ptbr_odd_man_out_v1", "probing_task"),
+        # ("dataset_wikipedia_ptbr_past_present_v1", "probing_task"),
+        # ("dataset_wikipedia_ptbr_sentence_length_v1", "probing_task"),
+        # ("dataset_wikipedia_ptbr_subj_number_v1", "probing_task"),
+        # ("dataset_wikipedia_ptbr_top_constituents_v1", "probing_task"),
+        # ("dataset_wikipedia_ptbr_tree_depth_v1", "probing_task"),
+        # ("dataset_wikipedia_ptbr_word_content_v1", "probing_task"),
+        # ("dataset_sp_court_cases_bigram_shift_v1", "probing_task"),
+        # ("dataset_sp_court_cases_coordination_inversion_v1", "probing_task"),
+        # ("dataset_sp_court_cases_obj_number_v1", "probing_task"),
+        # ("dataset_sp_court_cases_odd_man_out_v1", "probing_task"),
+        # ("dataset_sp_court_cases_past_present_v1", "probing_task"),
+        # ("dataset_sp_court_cases_sentence_length_v1", "probing_task"),
+        # ("dataset_sp_court_cases_subj_number_v1", "probing_task"),
+        # ("dataset_sp_court_cases_top_constituents_v1", "probing_task"),
+        # ("dataset_sp_court_cases_tree_depth_v1", "probing_task"),
+        # ("dataset_sp_court_cases_word_content_v1", "probing_task"),
+        # ("dataset_political_speeches_ptbr_bigram_shift_v1", "probing_task"),
+        # ("dataset_political_speeches_ptbr_coordination_inversion_v1", "probing_task"),
+        # ("dataset_political_speeches_ptbr_obj_number_v1", "probing_task"),
+        # ("dataset_political_speeches_ptbr_odd_man_out_v1", "probing_task"),
+        # ("dataset_political_speeches_ptbr_past_present_v1", "probing_task"),
+        # ("dataset_political_speeches_ptbr_sentence_length_v1", "probing_task"),
+        # ("dataset_political_speeches_ptbr_subj_number_v1", "probing_task"),
+        # ("dataset_political_speeches_ptbr_top_constituents_v1", "probing_task"),
+        # ("dataset_political_speeches_ptbr_tree_depth_v1", "probing_task"),
+        # ("dataset_political_speeches_ptbr_word_content_v1", "probing_task"),
+        # ("dataset_leg_pop_comments_ptbr_bigram_shift_v1", "probing_task"),
+        # ("dataset_leg_pop_comments_ptbr_coordination_inversion_v1", "probing_task"),
+        # ("dataset_leg_pop_comments_ptbr_obj_number_v1", "probing_task"),
+        # ("dataset_leg_pop_comments_ptbr_odd_man_out_v1", "probing_task"),
+        # ("dataset_leg_pop_comments_ptbr_past_present_v1", "probing_task"),
+        # ("dataset_leg_pop_comments_ptbr_sentence_length_v1", "probing_task"),
+        # ("dataset_leg_pop_comments_ptbr_subj_number_v1", "probing_task"),
+        # ("dataset_leg_pop_comments_ptbr_top_constituents_v1", "probing_task"),
+        # ("dataset_leg_pop_comments_ptbr_tree_depth_v1", "probing_task"),
+        # ("dataset_leg_pop_comments_ptbr_word_content_v1", "probing_task"),
+        # ("dataset_leg_docs_ptbr_bigram_shift_v1", "probing_task"),
+        # ("dataset_leg_docs_ptbr_coordination_inversion_v1", "probing_task"),
+        # ("dataset_leg_docs_ptbr_obj_number_v1", "probing_task"),
+        # ("dataset_leg_docs_ptbr_odd_man_out_v1", "probing_task"),
+        # ("dataset_leg_docs_ptbr_past_present_v1", "probing_task"),
+        # ("dataset_leg_docs_ptbr_sentence_length_v1", "probing_task"),
+        # ("dataset_leg_docs_ptbr_subj_number_v1", "probing_task"),
+        # ("dataset_leg_docs_ptbr_top_constituents_v1", "probing_task"),
+        # ("dataset_leg_docs_ptbr_tree_depth_v1", "probing_task"),
+        # ("dataset_leg_docs_ptbr_word_content_v1", "probing_task"),
     ],
 )
 def test_download_resources(resource_name: str, task_name: str):
@@ -102,8 +120,14 @@ def test_download_resources(resource_name: str, task_name: str):
 
     output_uri = os.path.join(output_dir, resource_name)
 
+    def check_file_exists(path: str) -> bool:
+        for ext in ("", ".pt", ".csv"):
+            if os.path.exists(f"{path}{ext}"):
+                return True
+        return False
+
     assert has_succeed
-    assert os.path.exists(output_uri) or os.path.exists(f"{output_uri}.pt")
+    assert check_file_exists(output_uri), output_uri
 
     t_start = time.perf_counter()
 
@@ -122,7 +146,7 @@ def test_download_resources(resource_name: str, task_name: str):
 
     assert t_delta <= 1.0, t_delta
     assert has_succeed
-    assert os.path.exists(output_uri) or os.path.exists(f"{output_uri}.pt"), output_uri
+    assert check_file_exists(output_uri), output_uri
 
     if os.path.isdir(output_uri):
         shutil.rmtree(output_dir)
@@ -181,7 +205,13 @@ def test_get_available_tasks():
 
 
 @pytest.mark.parametrize(
-    "task_name", ["legal_text_segmentation", "sentence_similarity", "probing_task"]
+    "task_name",
+    [
+        "legal_text_segmentation",
+        "sentence_similarity",
+        "probing_task",
+        "sentence_model_evaluation",
+    ],
 )
 def test_get_available_resources_for_segmentation_task(task_name: str):
     assert buscador.get_task_available_resources(task_name=task_name)
